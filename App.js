15e3e937af7e7e17d21eeb66073feb3325dba6fc
Abhishek K <abhishekakade@gmail.com>
2020-02-08 22:07:04 +0530

    feat: remove item

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import {uuid} from 'uuidv4';
+import ListItem from './components/ListItem';
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
       {/* <Text>Hello World!</Text> */}
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
